27e79d4d1d1a4306ba0779c1d882a56f674da278
alumno275ecb16 <brrachigo43@gmail.com>
2014-11-07 09:24:04 -0600

    1subida

---
+++ jquery-mobile/acciones.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+$(document).ready(function(e) {
+    document.addEventListener("deviceready",function(){
+		$('#disp table td').eg(3).text(device.model);
+		$('#disp table td').eg(5).text(device.cordova);
+		$('#disp table td').eg(7).text(device.platform);
+		$('#disp table td').eg(9).text(device.version);
+		$('#disp table td').eg(11).text(device.uuid);
+	document.addEventListener("pause",function(){
+		esccribehistoria('la app se pauso');
+	},false);//pause
+	document.addEventListener("resume",function(){
+		escribehistoria('la app se reinicio');
+	},false);//resume
+	document.addEventListener("online",function(){
+	},false);//se conecto
+	document.addEventListener("offline",function(){
+	},false);//se desconecto
+	},false);//ready device
+});//document
+function escribehistoria(accion){
+	$('#eHistoria').append('<li>'+accion+'</li>');
+}
